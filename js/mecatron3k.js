fe786a971509b4e28cc00421395f19a5c8cc502c
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2021-11-23 11:02:39 +0100

    Add files via upload

--- js/mecatron3k.js
+++ js/mecatron3k.js
@@ -9,11 +9,11 @@
 /**
   Controlador principal del juego.
 **/
-class Juego{
+class Juego {
   /**
     Constructor de la clase Juego
   **/
-  constructor(){
+  constructor() {
     this.vista = new Vista()
     this.modelo = new Modelo()
     this.generadorPalabras = null
@@ -24,7 +24,7 @@ class Juego{
   /**
     Pone en marcha el juego.
   **/
-  iniciar(){
+  iniciar() {
     console.log('Iniciando...')
     this.divPrincipal = document.getElementById('divPrincipal')
     this.vista.div = this.divPrincipal
@@ -32,102 +32,40 @@ class Juego{
     this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 300)
     window.onkeypress = this.pulsar.bind(this)
   }
-
-  generarPalabra(){
+  /**
+    Llama a vista y modelo para crear y mostrar una palabra nueva.
+   */
+  generarPalabra() {
     let nuevaPalabra = this.modelo.crearPalabra()
     this.vista.dibujar(nuevaPalabra)
   }
 
-  /**
-    Evento de atención a la pulsación del teclado.
-
-    Busca las palabras que tienen la letra pulsada y cambia su estado.
-    Cambiando el estilo y moviendo las letras de un sitio a otro.
-    @param {KeyboardEvent} evento Evento de pulsación del teclado.
-  **/
-  pulsar(evento){
+  pulsar(evento) {
     let letraPulsada = evento.key
     //Busco todas las palabras
     let palabras = this.divPrincipal.querySelectorAll('.palabra')
-    for(let palabra of palabras){
+    for (let palabra of palabras) {
       let span = palabra.children.item(0)
       let nodoTexto = palabra.childNodes[1]
       let textoRestante = nodoTexto.nodeValue
       let primeraLetraTextoRestante = textoRestante.charAt(0)
-      if (letraPulsada == primeraLetraTextoRestante){
+      if (letraPulsada == primeraLetraTextoRestante) {
         span.textContent += letraPulsada
         nodoTexto.nodeValue = textoRestante.substring(1)
 
-        //Si ha completado la palabra, la elimino y sumo puntos
-        if (nodoTexto.nodeValue.length == 0){
+        //Puntos y subida de nivel.
+        if (nodoTexto.nodeValue.length == 0) {
           palabra.remove()
           this.modelo.sumarPunto()
+          this.modelo.subirNivel()
         }
       }
-      else{
+      else {
         //Ha fallado, repongo el texto de la palabra
         nodoTexto.nodeValue = span.textContent + nodoTexto.nodeValue
         span.textContent = ''
       }
     }
-  }
-}
-
-/**
-  Clase Vista que muestra el juego.
-**/
-class Vista{
-  constructor(){
-    this.div = null   //Div donde se desarrolla el juego
-  }
-  /**
-    Dibuja el área de juego.
-    @param palabra {String} La nueva palabra.
-  */
-  dibujar(palabra){
-    // <div class=palabra>Meca</div>
-    let div = document.createElement('div')
-    this.div.appendChild(div)
-    let span = document.createElement('span')
-    div.appendChild(span)
-    div.appendChild(document.createTextNode(palabra))
-    div.classList.add('palabra')
-    div.style.top = '0px'
-    div.style.left = Math.floor(Math.random() * 85) + '%'
-  }
-  /**
-    Mueve las palabras del Juego
-  **/
-  moverPalabras(){
-    //Busco todas las palabras del div
-    let palabras = this.div.querySelectorAll('.palabra')
-
-    //Para cada palabra, aumento su atributo top.
-    for(let palabra of palabras){
-      let top = parseInt(palabra.style.top)
-      top += 5
-      palabra.style.top = `${top}px`
-      //Si ha llegado al final
-      if (top >= 760)
-        palabra.remove()
-    }
-  }
-}
-
-/**
-  Modelo de datos del juego.
-**/
-class Modelo{
-  constructor(){
-      this.palabras = ['En', 'un', 'lugar', 'de', 'La', 'Mancha']
-  }
-  /**
-    Devuelve una nueva palabra.
-    Devuelve aleatoriamente unn elemento del array de palabras.
-    @return {String} Palabra generada
-  **/
-  crearPalabra(){
-    return this.palabras[Math.floor(Math.random() * this.palabras.length)]
   }
 }
 
